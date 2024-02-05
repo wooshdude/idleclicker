2c3f85822736ff93ca1f49902a598923215705a5
colindurocher <lonepanda550@gmail.com>
2024-02-05 00:15:24 -0600

    Updated Button Loader
    fixed bug were multiple buttons would spawn

--- Store.gd
+++ Store.gd
@@ -14,9 +14,14 @@ func _process(delta):
 	LootBoxManager.loot_box_list
 
 
-func _on_v_box_container_visibility_changed():
-	if (v_box_container.visible == true):
-		for i in LootBoxManager.loot_box_list:
-			var new_button = LOOT_BOX.instantiate()
-			v_box_container.add_child(new_button)
-			new_button.load_button(i)
+
+func _on_visibility_changed():
+		if (visible == true):
+			for i in LootBoxManager.loot_box_list:
+				var new_button = LOOT_BOX.instantiate()
+				v_box_container.add_child(new_button)
+				new_button.load_button(i)
+		else:
+			for n in v_box_container.get_children():
+				v_box_container.remove_child(n)
+				n.queue_free()
